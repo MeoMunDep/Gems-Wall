653d978acb703b4dab14bb5e24ee3cdc51b6f8bb
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-07-11 02:30:52 +0700

    fix pack

--- setup.bat
+++ setup.bat
@@ -19,11 +19,11 @@ echo Checking dependencies...
 if exist "..\node_modules" (
     echo Using node_modules from parent directory...
     cd ..
-    CALL npm install --no-audit --no-fund --prefer-offline --force  user-agents axios colors https-proxy-agent socks-proxy-agent 
+    CALL npm install --no-audit --no-fund --prefer-offline --force  user-agents axios meo-forkcy-colors meo-forkcy-utils meo-forkcy-proxy
     cd %~dp0
 ) else (
     echo Installing dependencies in current directory...
-    CALL npm install --no-audit --no-fund --prefer-offline --force  user-agents axios colors https-proxy-agent socks-proxy-agent 
+    CALL npm install --no-audit --no-fund --prefer-offline --force  user-agents axios meo-forkcy-colors meo-forkcy-utils meo-forkcy-proxy
 )
 echo Dependencies installation completed!
 
